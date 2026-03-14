7471c02a02b28adc8d3e396bf07193831c71347f
Nurullaev Ilya <nurullaev567@gmail.com>
2026-03-14 19:11:06 +0500

    lesson1

---
+++ trello-client/src/pages/Login.tsx
@@ -0,0 +1,71 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Link } from '@tanstack/react-router';
+import { Eye, EyeOff } from 'lucide-react';
+import { useState, type FC } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+interface FormTypes{
+    username: string, 
+    password: string
+}
+
+export const Login: FC = () => {
+    const { control, handleSubmit } = useForm<FormTypes>();
+    // отображение пароля
+    const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className='flex min-h-screen items-center justify-center'>
+        <form className='flex flex-col gap-4 rounded-2xl p-4 border border-gray-200 w-96 bg-white/10' onSubmit={handleSubmit(data => console.log(data))}>
+            <h1 className='text-2xl font-bold text-center'>Login</h1>
+            <Controller
+                name='username'
+                control={control}
+                rules={{
+                    required: "Поле обязательно",
+                    validate: (value) => value.length > 4 || "Логин должен быть больше 4 символов"
+                }}
+                render={({field, fieldState: {error}}) => (
+                    <Input {...field} 
+                        error={!!error} 
+                        helperText={error?.message} 
+                        placeholder='Логин'
+                        variant="outlined"
+                    />
+                )}
+            />
+            <Controller
+                name='password'
+                control={control}
+                rules={{
+                    required: "Поле обязательно",
+                    validate: (value) => value.length > 4 || "Пароль должен быть больше 4 символов"
+                }}
+                render={({field, fieldState: {error}}) => (
+                    <div className='relative'>
+                        <Input {...field} 
+                            error={!!error} 
+                            helperText={error?.message} 
+                            placeholder='Пароль'
+                            variant="outlined"
+                            type={showPassword ? 'text' : 'password'}
+                        />
+                        <button
+                            type='button'
+                            onClick={() => setShowPassword(!showPassword)}
+                            className='absolute p-2.5 right-0 top-0 hover:bg-white/10 rounded-r-xl'
+                        >
+                            {showPassword ? <EyeOff size={20}/> : <Eye size={20}/>}
+                        </button>
+                    </div>
+                )}
+            />
+            <Button type='submit'>Войти</Button>
+            <Link to='/auth/register' className='text-sm underline text-center'>Зарегистрироваться</Link>
+        </form>
+    </div>
+  );
+};
+
+export default Login;
--- trello-client/src/routeTree.gen.ts
+++ trello-client/src/routeTree.gen.ts
@@ -10,33 +10,53 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
+import { Route as AuthIndexRouteImport } from './routes/auth/index'
+import { Route as AuthRegisterRouteImport } from './routes/auth/register'
 
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
+const AuthIndexRoute = AuthIndexRouteImport.update({
+  id: '/auth/',
+  path: '/auth/',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AuthRegisterRoute = AuthRegisterRouteImport.update({
+  id: '/auth/register',
+  path: '/auth/register',
+  getParentRoute: () => rootRouteImport,
+} as any)
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/auth/register': typeof AuthRegisterRoute
+  '/auth/': typeof AuthIndexRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/auth/register': typeof AuthRegisterRoute
+  '/auth': typeof AuthIndexRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/auth/register': typeof AuthRegisterRoute
+  '/auth/': typeof AuthIndexRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/auth/register' | '/auth/'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/auth/register' | '/auth'
+  id: '__root__' | '/' | '/auth/register' | '/auth/'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  AuthRegisterRoute: typeof AuthRegisterRoute
+  AuthIndexRoute: typeof AuthIndexRoute
 }
 
 declare module '@tanstack/react-router' {
@@ -48,11 +68,27 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
+    '/auth/': {
+      id: '/auth/'
+      path: '/auth'
+      fullPath: '/auth/'
+      preLoaderRoute: typeof AuthIndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/auth/register': {
+      id: '/auth/register'
+      path: '/auth/register'
+      fullPath: '/auth/register'
+      preLoaderRoute: typeof AuthRegisterRouteImport
+      parentRoute: typeof rootRouteImport
+    }
   }
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  AuthRegisterRoute: AuthRegisterRoute,
+  AuthIndexRoute: AuthIndexRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
